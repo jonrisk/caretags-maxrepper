ab63931c8abb68766999861c6b66dbe44cc513b3
Jon Skinner <jonskinner@Jons-MBP.fios-router.home>
2015-12-10 19:15:31 -0500

    First commit

---
+++ maxrepping.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+var hash = window.location.hash.substr(1);
+
+  var run;
+    for (run = 0; run < 10; run++) {
+      $("div[id= " + hash + "] .rate-good-icon a[title='Rate post']").click();
+    }
+
+      setTimeout(function(){
+        $("input[value='Rate']").click();
+      }, 7000);
+
+});
